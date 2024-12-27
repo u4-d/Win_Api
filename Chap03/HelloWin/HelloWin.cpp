809e8bc0e1601a63c40c97b9df4393d15c89727b
u4-d <isamasijp@yahoo.co.jp>
2024-12-27 20:48:15 +0900

    控件

--- Chap03/HelloWin/HelloWin.cpp
+++ Chap03/HelloWin/HelloWin.cpp
@@ -4,61 +4,49 @@
   ------------------------------------------------------------*/
 //#define UNICODE
 #include <windows.h>
+#include "resource.h"
 #pragma comment(lib,"winmm.lib") 
-LRESULT CALLBACK WndProc (HWND, UINT, WPARAM, LPARAM) ;
+// 全局变量:
+constexpr int MAX_LOADSTRING = 100;
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow)
 {
-     static TCHAR szAppName[] = TEXT ("HelloWin 标题") ;
-     HWND         hwnd ;
-     MSG          msg ;
-     WNDCLASS     wndclass ;
+    //避免提示未使用变量
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-     wndclass.style         = CS_HREDRAW | CS_VREDRAW ;
-     wndclass.lpfnWndProc   = WndProc ;
-     wndclass.cbClsExtra    = 0 ;
-     wndclass.cbWndExtra    = 0 ;
-     wndclass.hInstance     = hInstance ;
-     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION) ;
-     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW) ;
-     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH) ;
-     wndclass.lpszMenuName  = NULL ;
-     wndclass.lpszClassName = szAppName ;
+    // 使用 wcscpy_s 来复制字符串 
+    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"mainWindow");  
+    wcscpy_s(szTitle, MAX_LOADSTRING, L"szTitle - 窗口标题");
 
-     if (!RegisterClass (&wndclass))
-     {
-          MessageBox (NULL, TEXT ("This program requires Windows NT!"), 
-                      szAppName, MB_ICONERROR) ;
-          return 0 ;
-     }
-     
-     hwnd = CreateWindow (szAppName,                  // window class name
-                          TEXT ("The 中文Hello Program"), // window caption
-                          WS_OVERLAPPEDWINDOW,        // window style
-                          CW_USEDEFAULT,              // initial x position
-                          CW_USEDEFAULT,              // initial y position
-                          CW_USEDEFAULT,              // initial x size
-                          CW_USEDEFAULT,              // initial y size
-                          NULL,                       // parent window handle
-                          NULL,                       // window menu handle
-                          hInstance,                  // program instance handle
-                          NULL) ;                     // creation parameters
-     
-     ShowWindow (hwnd, nCmdShow) ;
-     UpdateWindow (hwnd) ;
-     
-     while (GetMessage (&msg, NULL, 0, 0))
-     {
-          TranslateMessage (&msg) ;
-          DispatchMessage (&msg) ;
-     }
-     return (int)msg.wParam ;
+    MyRegisterClass(hInstance);
+    // 执行应用程序初始化:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+    MSG          msg ;
+    // 主消息循环:
+    while (GetMessage (&msg, NULL, 0, 0))
+    {
+        TranslateMessage (&msg) ;
+        DispatchMessage (&msg) ;
+    }
+    return (int)msg.wParam ;
 }
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
      HDC         hdc ;
      PAINTSTRUCT ps ;
@@ -69,21 +57,96 @@ LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
      case WM_CREATE:
           //PlaySound (TEXT ("hellowin.wav"), NULL, SND_FILENAME | SND_ASYNC) ;
           return 0 ;
-          
+     case WM_COMMAND:
+     {
+         int wmId = LOWORD(wParam);
+         // 分析菜单选择:
+         switch (wmId)
+         {
+         case ID_ABOUT_INFO:
+             DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, About);
+             break;
+         case ID_NEW_EXIT:
+             DestroyWindow(hWnd);
+             break;
+         default:
+             return DefWindowProc(hWnd, message, wParam, lParam);
+         }
+     }
+     break;
      case WM_PAINT:
-          hdc = BeginPaint (hwnd, &ps) ;
+          hdc = BeginPaint (hWnd, &ps) ;
           
-          GetClientRect (hwnd, &rect) ;
+          GetClientRect (hWnd, &rect) ;
           
           DrawText (hdc, TEXT ("Hello, 中文Windows 98!"), -1, &rect,
                     DT_SINGLELINE | DT_CENTER | DT_VCENTER) ;
           
-          EndPaint (hwnd, &ps) ;
+          EndPaint (hWnd, &ps) ;
           return 0 ;
           
      case WM_DESTROY:
           PostQuitMessage (0) ;
           return 0 ;
      }
-     return DefWindowProc (hwnd, message, wParam, lParam) ;
+     return DefWindowProc (hWnd, message, wParam, lParam) ;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex = { 0 };  // 初始化为零，清空所有成员
+    wcex.cbSize = sizeof(WNDCLASSEXW);  // 设置结构体的大小
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;    
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDR_MENU1);;
+    //lpszClassName 未设置会注册失败
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));  // 加载大图标 IDI_APPLICATION
+    wcex.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));  // 加载小图标 IDI_INFORMATION
+
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // 将实例句柄存储在全局变量中
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
 }
---
+++ Chap03/HelloWin/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 HelloWin.rc 使用
+//
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
+#define IDR_MENU1                       103
+#define IDD_DIALOG1                     105
+#define IDC_RICHEDIT21                  1003
+#define IDC_DATETIMEPICKER1             1004
+#define ID_NEW_A                        40001
+#define ID_NEW_B                        40002
+#define ID_NEW_EXIT                     40003
+#define ID_ABOUT_INFO                   40004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
