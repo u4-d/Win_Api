f80285666888b54743c202c0671b0113d15c4ebe
u-Dev <isamasijp@yahoo.co.jp>
2024-12-29 15:07:45 +0900

    google format

--- Chap04/SysMets2/SysMets2.cpp
+++ Chap04/SysMets2/SysMets2.cpp
@@ -6,178 +6,171 @@
 constexpr int WINVER = 0x0500;
 // #define WINVER 0x0500
 #include <windows.h>
+
 #include "sysmets.h"
 
 // 全局变量:
 constexpr int MAX_LOADSTRING = 100;
-HINSTANCE hInst;                     // 当前实例
-WCHAR szTitle[MAX_LOADSTRING];       // 标题栏文本
-WCHAR szWindowClass[MAX_LOADSTRING]; // 主窗口类名
+HINSTANCE hInst;                      // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];        // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];  // 主窗口类名
 
 ATOM MyRegisterClass(HINSTANCE hInstance);
 BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
-                      _In_ int nCmdShow)
-{
-    // 避免提示未使用变量
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  // 避免提示未使用变量
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // 使用 wcscpy_s 来复制字符串
-    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"SysMets2");
-    wcscpy_s(szTitle, MAX_LOADSTRING, L"SysMets2");
+  // 使用 wcscpy_s 来复制字符串
+  wcscpy_s(szWindowClass, MAX_LOADSTRING, L"SysMets2");
+  wcscpy_s(szTitle, MAX_LOADSTRING, L"SysMets2");
 
-    MyRegisterClass(hInstance);
-    // 执行应用程序初始化:
-    if (!InitInstance(hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-    MSG msg;
+  MyRegisterClass(hInstance);
+  // 执行应用程序初始化:
+  if (!InitInstance(hInstance, nCmdShow)) {
+    return FALSE;
+  }
+  MSG msg;
 
-     while (GetMessage (&msg, NULL, 0, 0))
-     {
-          TranslateMessage (&msg) ;
-          DispatchMessage (&msg) ;
-     }
-     return (int)msg.wParam ;
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return (int)msg.wParam;
 }
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-     static int  cxChar, cxCaps, cyChar, cyClient, iVscrollPos ;
-     HDC         hdc ;
-     int         i, y ;
-     PAINTSTRUCT ps ;
-     TCHAR       szBuffer[10]={0} ;
-     TEXTMETRIC  tm ;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam,
+                         LPARAM lParam) {
+  static int cxChar, cxCaps, cyChar, cyClient, iVscrollPos;
+  HDC hdc;
+  int i, y;
+  PAINTSTRUCT ps;
+  TCHAR szBuffer[10] = {0};
+  TEXTMETRIC tm;
 
-     switch (message)
-     {
-     case WM_CREATE:
-          hdc = GetDC (hwnd) ;
+  switch (message) {
+    case WM_CREATE:
+      hdc = GetDC(hwnd);
 
-          GetTextMetrics (hdc, &tm) ;
-          cxChar = tm.tmAveCharWidth ;
-          cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2 ;
-          cyChar = tm.tmHeight + tm.tmExternalLeading ;
+      GetTextMetrics(hdc, &tm);
+      cxChar = tm.tmAveCharWidth;
+      cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+      cyChar = tm.tmHeight + tm.tmExternalLeading;
 
-          ReleaseDC (hwnd, hdc) ;
+      ReleaseDC(hwnd, hdc);
 
-          SetScrollRange (hwnd, SB_VERT, 0, NUMLINES - 1, FALSE) ;
-          SetScrollPos   (hwnd, SB_VERT, iVscrollPos, TRUE) ;
-          return 0 ;
+      SetScrollRange(hwnd, SB_VERT, 0, NUMLINES - 1, FALSE);
+      SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+      return 0;
 
-     case WM_SIZE:
-          cyClient = HIWORD (lParam) ;
-          return 0 ;
+    case WM_SIZE:
+      cyClient = HIWORD(lParam);
+      return 0;
 
-     case WM_VSCROLL:
-          switch (LOWORD (wParam))
-          {
-          case SB_LINEUP:
-               iVscrollPos -= 1 ;
-               break ;
-     
-          case SB_LINEDOWN:
-               iVscrollPos += 1 ;
-               break ;
-     
-          case SB_PAGEUP:
-               iVscrollPos -= cyClient / cyChar ;
-               break ;
-     
-          case SB_PAGEDOWN:
-               iVscrollPos += cyClient / cyChar ;
-               break ;
-     
-          case SB_THUMBPOSITION:
-               iVscrollPos = HIWORD (wParam) ;
-               break ;
-     
-          default :
-               break ;
-          }
+    case WM_VSCROLL:
+      switch (LOWORD(wParam)) {
+        case SB_LINEUP:
+          iVscrollPos -= 1;
+          break;
 
-          iVscrollPos = max (0, min (iVscrollPos, NUMLINES - 1)) ;
+        case SB_LINEDOWN:
+          iVscrollPos += 1;
+          break;
 
-          if (iVscrollPos != GetScrollPos (hwnd, SB_VERT))
-          {
-               SetScrollPos (hwnd, SB_VERT, iVscrollPos, TRUE) ;
-               InvalidateRect (hwnd, NULL, TRUE) ;
-          }
-          return 0 ;
+        case SB_PAGEUP:
+          iVscrollPos -= cyClient / cyChar;
+          break;
 
-     case WM_PAINT:
-          hdc = BeginPaint (hwnd, &ps) ;
-     
-          for (i = 0 ; i < NUMLINES ; i++)
-          {
-               y = cyChar * (i - iVscrollPos) ;
-     
-               TextOut (hdc, 0, y,
-                        sysmetrics[i].szLabel,
-                        lstrlen (sysmetrics[i].szLabel)) ;
-     
-               TextOut (hdc, 22 * cxCaps, y,
-                        sysmetrics[i].szDesc,
-                        lstrlen (sysmetrics[i].szDesc)) ;
-     
-               SetTextAlign (hdc, TA_RIGHT | TA_TOP) ;
-     
-               TextOut (hdc, 22 * cxCaps + 40 * cxChar, y, szBuffer,
-                        wsprintf (szBuffer, TEXT ("%5d"),
-                             GetSystemMetrics (sysmetrics[i].iIndex))) ;
-     
-               SetTextAlign (hdc, TA_LEFT | TA_TOP) ;
-          }
-          EndPaint (hwnd, &ps) ;
-          return 0 ;
+        case SB_PAGEDOWN:
+          iVscrollPos += cyClient / cyChar;
+          break;
 
-     case WM_DESTROY:
-          PostQuitMessage (0) ;
-          return 0 ;
-     }
-     return DefWindowProc (hwnd, message, wParam, lParam) ;
+        case SB_THUMBPOSITION:
+          iVscrollPos = HIWORD(wParam);
+          break;
+
+        default:
+          break;
+      }
+
+      iVscrollPos = max(0, min(iVscrollPos, NUMLINES - 1));
+
+      if (iVscrollPos != GetScrollPos(hwnd, SB_VERT)) {
+        SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+        InvalidateRect(hwnd, NULL, TRUE);
+      }
+      return 0;
+
+    case WM_PAINT:
+      hdc = BeginPaint(hwnd, &ps);
+
+      for (i = 0; i < NUMLINES; i++) {
+        y = cyChar * (i - iVscrollPos);
+
+        TextOut(hdc, 0, y, sysmetrics[i].szLabel,
+                lstrlen(sysmetrics[i].szLabel));
+
+        TextOut(hdc, 22 * cxCaps, y, sysmetrics[i].szDesc,
+                lstrlen(sysmetrics[i].szDesc));
+
+        SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+
+        TextOut(hdc, 22 * cxCaps + 40 * cxChar, y, szBuffer,
+                wsprintf(szBuffer, TEXT("%5d"),
+                         GetSystemMetrics(sysmetrics[i].iIndex)));
+
+        SetTextAlign(hdc, TA_LEFT | TA_TOP);
+      }
+      EndPaint(hwnd, &ps);
+      return 0;
+
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+  }
+  return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex = {0};            // 初始化为零，清空所有成员
-    wcex.cbSize = sizeof(WNDCLASSEXW); // 设置结构体的大小
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+  WNDCLASSEXW wcex = {0};             // 初始化为零，清空所有成员
+  wcex.cbSize = sizeof(WNDCLASSEXW);  // 设置结构体的大小
 
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    // lpszClassName 未设置会注册失败
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);   // 加载大图标 IDI_APPLICATION
-    wcex.hIconSm = LoadIcon(hInstance, IDI_INFORMATION); // 加载小图标 IDI_INFORMATION
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = NULL;
+  // lpszClassName 未设置会注册失败
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIcon =
+      LoadIcon(hInstance, IDI_APPLICATION);  // 加载大图标 IDI_APPLICATION
+  wcex.hIconSm =
+      LoadIcon(hInstance, IDI_INFORMATION);  // 加载小图标 IDI_INFORMATION
 
-    return RegisterClassExW(&wcex);
+  return RegisterClassExW(&wcex);
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-    hInst = hInstance; // 将实例句柄存储在全局变量中
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+  hInst = hInstance;  // 将实例句柄存储在全局变量中
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
-                              nullptr, hInstance, nullptr);
+  HWND hWnd =
+      CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                    0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-    if (!hWnd)
-    {
-        return FALSE;
-    }
+  if (!hWnd) {
+    return FALSE;
+  }
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
-    return TRUE;
+  return TRUE;
 }
--- Chap04/SysMets3/SysMets3.cpp
+++ Chap04/SysMets3/SysMets3.cpp
@@ -1,299 +1,287 @@
 /*----------------------------------------------------
-   SYSMETS3.C -- System Metrics Display Program No. 3
+   SYSMETS1.C -- System Metrics Display Program No. 1
                  (c) Charles Petzold, 1998
   ----------------------------------------------------*/
 constexpr int WINVER = 0x0500;
 // #define WINVER 0x0500
 #include <windows.h>
+
 #include "sysmets.h"
 
 // 全局变量:
 constexpr int MAX_LOADSTRING = 100;
-HINSTANCE hInst;                     // 当前实例
-WCHAR szTitle[MAX_LOADSTRING];       // 标题栏文本
-WCHAR szWindowClass[MAX_LOADSTRING]; // 主窗口类名
+HINSTANCE hInst;                      // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];        // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];  // 主窗口类名
 
 ATOM MyRegisterClass(HINSTANCE hInstance);
 BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
-                      _In_ int nCmdShow)
-{
-    // 避免提示未使用变量
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  // 避免提示未使用变量
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // 使用 wcscpy_s 来复制字符串
-    wcscpy_s(szWindowClass, MAX_LOADSTRING, L"SysMets2");
-    wcscpy_s(szTitle, MAX_LOADSTRING, L"SysMets2");
+  // 使用 wcscpy_s 来复制字符串
+  wcscpy_s(szWindowClass, MAX_LOADSTRING, L"SysMets2");
+  wcscpy_s(szTitle, MAX_LOADSTRING, L"SysMets2");
 
-    MyRegisterClass(hInstance);
-    // 执行应用程序初始化:
-    if (!InitInstance(hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-    MSG msg;
-     
-     while (GetMessage (&msg, NULL, 0, 0))
-     {
-          TranslateMessage (&msg) ;
-          DispatchMessage (&msg) ;
-     }
-     return (int)msg.wParam ;
+  MyRegisterClass(hInstance);
+  // 执行应用程序初始化:
+  if (!InitInstance(hInstance, nCmdShow)) {
+    return FALSE;
+  }
+  MSG msg;
+
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return (int)msg.wParam;
 }
 
-LRESULT CALLBACK WndProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-     static int  cxChar, cxCaps, cyChar, cxClient, cyClient, iMaxWidth ;
-     HDC         hdc ;
-     int         i, x, y, iVertPos, iHorzPos, iPaintBeg, iPaintEnd ;
-     PAINTSTRUCT ps ;
-     SCROLLINFO  si ;
-     TCHAR       szBuffer[10]={0} ;
-     TEXTMETRIC  tm ;
-     
-     switch (message)
-     {
-     case WM_CREATE:
-          hdc = GetDC (hwnd) ;
-          
-          GetTextMetrics (hdc, &tm) ;
-          cxChar = tm.tmAveCharWidth ;
-          cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2 ;
-          cyChar = tm.tmHeight + tm.tmExternalLeading ;
-          
-          ReleaseDC (hwnd, hdc) ;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam,
+                         LPARAM lParam) {
+  static int cxChar, cxCaps, cyChar, cxClient, cyClient, iMaxWidth;
+  HDC hdc;
+  int i, x, y, iVertPos, iHorzPos, iPaintBeg, iPaintEnd;
+  PAINTSTRUCT ps;
+  SCROLLINFO si;
+  TCHAR szBuffer[10] = {0};
+  TEXTMETRIC tm;
 
-               // Save the width of the three columns
-          
-          iMaxWidth = 40 * cxChar + 22 * cxCaps ;
-          return 0 ;
-          
-     case WM_SIZE:
-          cxClient = LOWORD (lParam) ;
-          cyClient = HIWORD (lParam) ;
+  switch (message) {
+    case WM_CREATE:
+      hdc = GetDC(hwnd);
 
-               // Set vertical scroll bar range and page size
+      GetTextMetrics(hdc, &tm);
+      cxChar = tm.tmAveCharWidth;
+      cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+      cyChar = tm.tmHeight + tm.tmExternalLeading;
 
-          si.cbSize = sizeof (si) ;
-          si.fMask  = SIF_RANGE | SIF_PAGE ;
-          si.nMin   = 0 ;
-          si.nMax   = NUMLINES - 1 ;
-          si.nPage  = cyClient / cyChar ;
-          SetScrollInfo (hwnd, SB_VERT, &si, TRUE) ;
+      ReleaseDC(hwnd, hdc);
 
-               // Set horizontal scroll bar range and page size
+      // Save the width of the three columns
 
-          si.cbSize = sizeof (si) ;
-          si.fMask  = SIF_RANGE | SIF_PAGE ;
-          si.nMin   = 0 ;
-          si.nMax   = 2 + iMaxWidth / cxChar ;
-          si.nPage  = cxClient / cxChar ;
-          SetScrollInfo (hwnd, SB_HORZ, &si, TRUE) ;
-          return 0 ;
-          
-     case WM_VSCROLL:
-               // Get all the vertial scroll bar information
+      iMaxWidth = 40 * cxChar + 22 * cxCaps;
+      return 0;
 
-          si.cbSize = sizeof (si) ;
-          si.fMask  = SIF_ALL ;
-          GetScrollInfo (hwnd, SB_VERT, &si) ;
+    case WM_SIZE:
+      cxClient = LOWORD(lParam);
+      cyClient = HIWORD(lParam);
 
-               // Save the position for comparison later on
+      // Set vertical scroll bar range and page size
 
-          iVertPos = si.nPos ;
+      si.cbSize = sizeof(si);
+      si.fMask = SIF_RANGE | SIF_PAGE;
+      si.nMin = 0;
+      si.nMax = NUMLINES - 1;
+      si.nPage = cyClient / cyChar;
+      SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
 
-          switch (LOWORD (wParam))
-          {
-          case SB_TOP:
-               si.nPos = si.nMin ;
-               break ;
-               
-          case SB_BOTTOM:
-               si.nPos = si.nMax ;
-               break ;
-               
-          case SB_LINEUP:
-               si.nPos -= 1 ;
-               break ;
-               
-          case SB_LINEDOWN:
-               si.nPos += 1 ;
-               break ;
-               
-          case SB_PAGEUP:
-               si.nPos -= si.nPage ;
-               break ;
-               
-          case SB_PAGEDOWN:
-               si.nPos += si.nPage ;
-               break ;
-               
-          case SB_THUMBTRACK:
-               si.nPos = si.nTrackPos ;
-               break ;
-               
-          default:
-               break ;         
-          }
-               // Set the position and then retrieve it.  Due to adjustments
-               //   by Windows it may not be the same as the value set.
+      // Set horizontal scroll bar range and page size
 
-          si.fMask = SIF_POS ;
-          SetScrollInfo (hwnd, SB_VERT, &si, TRUE) ;
-          GetScrollInfo (hwnd, SB_VERT, &si) ;
+      si.cbSize = sizeof(si);
+      si.fMask = SIF_RANGE | SIF_PAGE;
+      si.nMin = 0;
+      si.nMax = 2 + iMaxWidth / cxChar;
+      si.nPage = cxClient / cxChar;
+      SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+      return 0;
 
-               // If the position has changed, scroll the window and update it
+    case WM_VSCROLL:
+      // Get all the vertial scroll bar information
 
-          if (si.nPos != iVertPos)
-          {                    
-               ScrollWindow (hwnd, 0, cyChar * (iVertPos - si.nPos), 
-                                   NULL, NULL) ;
-               UpdateWindow (hwnd) ;
-          }
-          return 0 ;
-          
-     case WM_HSCROLL:
-               // Get all the vertial scroll bar information
+      si.cbSize = sizeof(si);
+      si.fMask = SIF_ALL;
+      GetScrollInfo(hwnd, SB_VERT, &si);
 
-          si.cbSize = sizeof (si) ;
-          si.fMask  = SIF_ALL ;
+      // Save the position for comparison later on
 
-               // Save the position for comparison later on
+      iVertPos = si.nPos;
 
-          GetScrollInfo (hwnd, SB_HORZ, &si) ;
-          iHorzPos = si.nPos ;
+      switch (LOWORD(wParam)) {
+        case SB_TOP:
+          si.nPos = si.nMin;
+          break;
 
-          switch (LOWORD (wParam))
-          {
-          case SB_LINELEFT:
-               si.nPos -= 1 ;
-               break ;
-               
-          case SB_LINERIGHT:
-               si.nPos += 1 ;
-               break ;
-               
-          case SB_PAGELEFT:
-               si.nPos -= si.nPage ;
-               break ;
-               
-          case SB_PAGERIGHT:
-               si.nPos += si.nPage ;
-               break ;
-               
-          case SB_THUMBPOSITION:
-               si.nPos = si.nTrackPos ;
-               break ;
-               
-          default :
-               break ;
-          }
-               // Set the position and then retrieve it.  Due to adjustments
-               //   by Windows it may not be the same as the value set.
+        case SB_BOTTOM:
+          si.nPos = si.nMax;
+          break;
 
-          si.fMask = SIF_POS ;
-          SetScrollInfo (hwnd, SB_HORZ, &si, TRUE) ;
-          GetScrollInfo (hwnd, SB_HORZ, &si) ;
-          
-               // If the position has changed, scroll the window 
+        case SB_LINEUP:
+          si.nPos -= 1;
+          break;
 
-          if (si.nPos != iHorzPos)
-          {
-               ScrollWindow (hwnd, cxChar * (iHorzPos - si.nPos), 0, 
-                             NULL, NULL) ;
-          }
-          return 0 ;
+        case SB_LINEDOWN:
+          si.nPos += 1;
+          break;
 
-     case WM_PAINT :
-          hdc = BeginPaint (hwnd, &ps) ;
+        case SB_PAGEUP:
+          si.nPos -= si.nPage;
+          break;
 
-               // Get vertical scroll bar position
+        case SB_PAGEDOWN:
+          si.nPos += si.nPage;
+          break;
 
-          si.cbSize = sizeof (si) ;
-          si.fMask  = SIF_POS ;
-          GetScrollInfo (hwnd, SB_VERT, &si) ;
-          iVertPos = si.nPos ;
+        case SB_THUMBTRACK:
+          si.nPos = si.nTrackPos;
+          break;
 
-               // Get horizontal scroll bar position
+        default:
+          break;
+      }
+      // Set the position and then retrieve it.  Due to adjustments
+      //   by Windows it may not be the same as the value set.
 
-          GetScrollInfo (hwnd, SB_HORZ, &si) ;
-          iHorzPos = si.nPos ;
+      si.fMask = SIF_POS;
+      SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+      GetScrollInfo(hwnd, SB_VERT, &si);
 
-               // Find painting limits
+      // If the position has changed, scroll the window and update it
 
-          iPaintBeg = max (0, iVertPos + ps.rcPaint.top / cyChar) ;
-          iPaintEnd = min (NUMLINES - 1,
-                           iVertPos + ps.rcPaint.bottom / cyChar) ;
-          
-          for (i = iPaintBeg ; i <= iPaintEnd ; i++)
-          {
-               x = cxChar * (1 - iHorzPos) ;
-               y = cyChar * (i - iVertPos) ;
-               
-               TextOut (hdc, x, y,
-                        sysmetrics[i].szLabel,
-                        lstrlen (sysmetrics[i].szLabel)) ;
-               
-               TextOut (hdc, x + 22 * cxCaps, y,
-                        sysmetrics[i].szDesc,
-                        lstrlen (sysmetrics[i].szDesc)) ;
-               
-               SetTextAlign (hdc, TA_RIGHT | TA_TOP) ;
-               
-               TextOut (hdc, x + 22 * cxCaps + 40 * cxChar, y, szBuffer,
-                        wsprintf (szBuffer, TEXT ("%5d"),
-                             GetSystemMetrics (sysmetrics[i].iIndex))) ;
-               
-               SetTextAlign (hdc, TA_LEFT | TA_TOP) ;
-          }
+      if (si.nPos != iVertPos) {
+        ScrollWindow(hwnd, 0, cyChar * (iVertPos - si.nPos), NULL, NULL);
+        UpdateWindow(hwnd);
+      }
+      return 0;
 
-          EndPaint (hwnd, &ps) ;
-          return 0 ;
-          
-     case WM_DESTROY :
-          PostQuitMessage (0) ;
-          return 0 ;
-     }
-     return DefWindowProc (hwnd, message, wParam, lParam) ;
+    case WM_HSCROLL:
+      // Get all the vertial scroll bar information
+
+      si.cbSize = sizeof(si);
+      si.fMask = SIF_ALL;
+
+      // Save the position for comparison later on
+
+      GetScrollInfo(hwnd, SB_HORZ, &si);
+      iHorzPos = si.nPos;
+
+      switch (LOWORD(wParam)) {
+        case SB_LINELEFT:
+          si.nPos -= 1;
+          break;
+
+        case SB_LINERIGHT:
+          si.nPos += 1;
+          break;
+
+        case SB_PAGELEFT:
+          si.nPos -= si.nPage;
+          break;
+
+        case SB_PAGERIGHT:
+          si.nPos += si.nPage;
+          break;
+
+        case SB_THUMBPOSITION:
+          si.nPos = si.nTrackPos;
+          break;
+
+        default:
+          break;
+      }
+      // Set the position and then retrieve it.  Due to adjustments
+      //   by Windows it may not be the same as the value set.
+
+      si.fMask = SIF_POS;
+      SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+      GetScrollInfo(hwnd, SB_HORZ, &si);
+
+      // If the position has changed, scroll the window
+
+      if (si.nPos != iHorzPos) {
+        ScrollWindow(hwnd, cxChar * (iHorzPos - si.nPos), 0, NULL, NULL);
+      }
+      return 0;
+
+    case WM_PAINT:
+      hdc = BeginPaint(hwnd, &ps);
+
+      // Get vertical scroll bar position
+
+      si.cbSize = sizeof(si);
+      si.fMask = SIF_POS;
+      GetScrollInfo(hwnd, SB_VERT, &si);
+      iVertPos = si.nPos;
+
+      // Get horizontal scroll bar position
+
+      GetScrollInfo(hwnd, SB_HORZ, &si);
+      iHorzPos = si.nPos;
+
+      // Find painting limits
+
+      iPaintBeg = max(0, iVertPos + ps.rcPaint.top / cyChar);
+      iPaintEnd = min(NUMLINES - 1, iVertPos + ps.rcPaint.bottom / cyChar);
+
+      for (i = iPaintBeg; i <= iPaintEnd; i++) {
+        x = cxChar * (1 - iHorzPos);
+        y = cyChar * (i - iVertPos);
+
+        TextOut(hdc, x, y, sysmetrics[i].szLabel,
+                lstrlen(sysmetrics[i].szLabel));
+
+        TextOut(hdc, x + 22 * cxCaps, y, sysmetrics[i].szDesc,
+                lstrlen(sysmetrics[i].szDesc));
+
+        SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+
+        TextOut(hdc, x + 22 * cxCaps + 40 * cxChar, y, szBuffer,
+                wsprintf(szBuffer, TEXT("%5d"),
+                         GetSystemMetrics(sysmetrics[i].iIndex)));
+
+        SetTextAlign(hdc, TA_LEFT | TA_TOP);
+      }
+
+      EndPaint(hwnd, &ps);
+      return 0;
+
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+  }
+  return DefWindowProc(hwnd, message, wParam, lParam);
 }
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex = {0};            // 初始化为零，清空所有成员
-    wcex.cbSize = sizeof(WNDCLASSEXW); // 设置结构体的大小
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+  WNDCLASSEXW wcex = {0};             // 初始化为零，清空所有成员
+  wcex.cbSize = sizeof(WNDCLASSEXW);  // 设置结构体的大小
 
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    // lpszClassName 未设置会注册失败
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);   // 加载大图标 IDI_APPLICATION
-    wcex.hIconSm = LoadIcon(hInstance, IDI_INFORMATION); // 加载小图标 IDI_INFORMATION
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = NULL;
+  // lpszClassName 未设置会注册失败
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIcon =
+      LoadIcon(hInstance, IDI_APPLICATION);  // 加载大图标 IDI_APPLICATION
+  wcex.hIconSm =
+      LoadIcon(hInstance, IDI_INFORMATION);  // 加载小图标 IDI_INFORMATION
 
-    return RegisterClassExW(&wcex);
+  return RegisterClassExW(&wcex);
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-    hInst = hInstance; // 将实例句柄存储在全局变量中
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+  hInst = hInstance;  // 将实例句柄存储在全局变量中
 
-    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr,
-                              nullptr, hInstance, nullptr);
+  HWND hWnd =
+      CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                    0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-    if (!hWnd)
-    {
-        return FALSE;
-    }
+  if (!hWnd) {
+    return FALSE;
+  }
 
-    ShowWindow(hWnd, nCmdShow);
-    UpdateWindow(hWnd);
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
 
-    return TRUE;
+  return TRUE;
 }
