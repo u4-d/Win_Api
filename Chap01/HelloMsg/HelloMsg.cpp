99d5b7a30e0a3e5d8c932c137af592711d419597
u-DeskTop <isamasijp@yahoo.co.jp>
2025-01-11 10:28:12 +0900

    备份 .vscode目录

--- Chap01/HelloMsg/HelloMsg.cpp
+++ Chap01/HelloMsg/HelloMsg.cpp
@@ -2,12 +2,12 @@
    HelloMsg.c -- Displays "Hello, Windows 98!" in a message box
                  (c) Charles Petzold, 1998
   --------------------------------------------------------------*/
-
 #include <windows.h>
 
-//int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,_In_ int nCmdShow)
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR szCmdLine, _In_ int iCmdShow)
-{
+// int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE
+// hPrevInstance, _In_ LPWSTR lpCmdLine,_In_ int nCmdShow)
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+                    _In_ LPWSTR szCmdLine, _In_ int iCmdShow) {
     MessageBox(NULL, TEXT("Hello, 中文 Windows 98!"), TEXT("HelloMsg"), 0);
 
     return 0;
